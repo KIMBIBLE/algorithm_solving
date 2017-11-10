d2cc1ea675066a5794f715de61a63963d4c28481
bbkim <jotun9935@gmail.com>
2017-11-10 02:27:41 +0000

    Solve : baekjoonOJ 2741 N찍기

---
+++ baekjoonOJ/2731.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	int N;	// N : Natural Number
+	cin >> N;
+
+	
+	for(int i = 1; i <= N; i++){
+		//cout << i << endl;
+		printf("%d\n", i);
+	}
+}
