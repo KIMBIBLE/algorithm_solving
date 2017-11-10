3709a8e9f726882394a87368e3ccf5faf2ccda91
bbkim <jotun9935@gmail.com>
2017-11-10 02:29:06 +0000

    Solve : baekjoonOJ 10869 사칙연산

---
+++ baekjoonOJ/10869.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	int A, B;
+	cin >> A >> B;
+	cout << A + B << endl;
+	cout << A - B << endl;
+	cout << A * B << endl;
+	cout << A / B << endl;
+	cout << A % B << endl;
+}
