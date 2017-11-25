9ad2cecee1db82d52335f81a603af086605bc215
bbkim <jotun9935@gmail.com>
2017-11-25 17:18:12 +0000

    solve

---
+++ baekjoonOJ/10172.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    string msg = "";
+	msg += "|\\_/|";
+	msg += "\n";
+	
+	msg += "|q p|   /}";
+	msg += "\n";
+	
+	msg += "( 0 )\"\"\"\\";
+	msg += "\n";
+	
+	msg += "|\"^\"`    |";
+	msg += "\n"; 
+	
+	msg += "||_/=\\\\__|";
+	msg += "\n";
+	cout << msg;
+}
