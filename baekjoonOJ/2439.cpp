37e165a64c8404b5b7e162adad5cddef82e0fa06
bbkim <jotun9935@gmail.com>
2017-11-28 10:17:50 +0000

    solve

--- baekjoonOJ/2439.cpp
+++ baekjoonOJ/2439.cpp
@@ -7,9 +7,9 @@ int main(int argc, char* argv[])
 	int N;
 	cin >> N;
 	
-	for(int i=5; i>=1; i--)
+	for(int i=N; i>=1; i--)
 	{
-		for(int j=1; j<=5; j++)
+		for(int j=1; j<=N; j++)
 		{
 			if(i <= j)
 				printf("*");
