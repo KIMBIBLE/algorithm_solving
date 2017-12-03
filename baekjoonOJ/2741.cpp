d6688bd1c1cfcacd1bba83bf6957707ad99c890d
bbkim <jotun9935@gmail.com>
2017-12-03 05:41:29 +0000

    solve : 'endl' is too slow than '\n'

---
+++ baekjoonOJ/2741.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    int num;
+    cin >> num;
+    for(int i=1; i<=num; i++)
+    {
+        cout << i << '\n';
+    }
+}
