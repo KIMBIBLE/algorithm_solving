aaccaa05838eb652882cc62acb51b5ecbd423e7d
bbkim <jotun9935@gmail.com>
2018-03-25 23:49:52 +0900

    solve

---
+++ codeplus/11021.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    for(int i=1; i<=t; i++){
+        int a, b;
+        cin >> a >> b;
+        cout << "Case #" << i << ": " << a+b << '\n';
+    }
+}
