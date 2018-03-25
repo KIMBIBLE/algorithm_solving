0a452b4f48021af2d125237ab1a41e30c4bcb028
bbkim <jotun9935@gmail.com>
2018-03-25 23:33:32 +0900

    solve

---
+++ codeplus/10950.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    int t;
+    cin >> t;
+    while(t--){
+        int a, b;
+        cin >> a >> b;
+        cout << a + b << '\n';
+    }
+}
