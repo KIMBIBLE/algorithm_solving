35e6b822caaaab83a132201e6862005024fe9554
bbkim <jotun9935@gmail.com>
2018-03-25 23:51:50 +0900

    solve

---
+++ codeplus/11022.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    for(int i=1; i<=t; i++){
+        int a, b;
+        cin >> a >> b;
+        printf("Case #%d: %d + %d = %d", i, a, b, a+b);
+    }
+}
