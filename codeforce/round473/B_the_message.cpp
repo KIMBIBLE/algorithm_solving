8c5e6192ad041f6c5209adee58aedae53b758614
bbkim <jotun9935@gmail.com>
2018-04-16 18:03:23 +0900

    B : time limit

--- codeforce/round473/B_the_message.cpp
+++ codeforce/round473/B_the_message.cpp
@@ -8,7 +8,7 @@ int main()
 {
     int n, k, m;
     cin >> n >> k >> m;
-    vector<pair<string, int>> v(n+1);
+    vector<pair<string, unsigned long long>> v(n+1);
 
     for(int i=1; i<=n; i++){
         cin >> v[i].first;
@@ -21,28 +21,25 @@ int main()
     for(int i=1; i<=k; i++){
         int cnt;
         cin >> cnt;
+        unsigned long long min = -1; // max value
         if(cnt == 1){
             cin >> cnt; // meaningless input
         }
         else{
             vector<int> tmp(cnt);
-
+            v[0].second = -1;
             for(int j=0; j<cnt; j++){
                 cin >> tmp[j];
-            }
-            
-            int min = v[tmp[0]].second;
-            for(int j=0; j<cnt; j++){
                 if(min > v[tmp[j]].second) min = v[tmp[j]].second;
             }
-
+            
             for(int j=0; j<cnt; j++){
                 v[tmp[j]].second = min;
             }
         }
     }
 
-    int sum = 0;
+    unsigned long long sum = 0;
     string tmp;
     for(int i=0; i<m; i++){
         cin >> tmp;
