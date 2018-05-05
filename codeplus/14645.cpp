5837a836aba9dc3ec6ced9a93dd36877a44b946a
Oss9935 <jotun9935@gmail.com>
2018-05-05 16:06:40 +0900

    solve

---
+++ codeplus/14645.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, k; cin >> n >> k;
+    while(n--){
+        int t1, t2;
+        cin >> t1 >> t2;
+    }
+    cout << "비와이";
+}
