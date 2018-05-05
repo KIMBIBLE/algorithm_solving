f457605593e7418f14d7502b4fbeea38aec30470
Oss9935 <jotun9935@gmail.com>
2018-05-05 15:10:38 +0900

    solve

---
+++ codeplus/5339.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string s = "";
+    s += "     /~\\\n";
+    s += "    ( oo|\n";
+    s += "    _\\=/_\n";
+    s += "   /  _  \\\n";
+    s += "  //|/.\\|\\\\\n";
+    s += " ||  \\ /  ||\n";
+    s += "============\n";
+    s += "|          |\n";
+    s += "|          |\n";
+    s += "|          |\n";
+
+    cout << s;
+
+}
