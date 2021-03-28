9d48bfab1d8fbe0f0e69863f22c6497aa1aba1de
Sungkyung Kim <jotun9935@gmail.com>
2021-03-29 00:06:44 +0900

    solve ('bfs_queue, dfs_stack' with adjancy matrix)

--- baekjoonOJ/1260.py
+++ baekjoonOJ/1260.py
@@ -1,31 +1,43 @@
-VISITED = -1
 CONNECTED = 1
 
+
 def solution(n, m, v, g):
-    dfs_res = 0
-    bfs_res = 0
+    #print(*_dfs(v, [], g))
+    print(*dfs(n, m, v, g))
+    print(*bfs(n, m, v, g))
 
 def dfs(n, m, v, g):
-    
+    visited_node = []
+    stack = [v]
+
+    while stack:
+        cur = stack[len(stack) - 1] # stack top
+        if cur not in visited_node:
+            visited_node.append(cur)
+
+        is_find = False
+        for i in range(0, n + 1):
+            if g[cur][i] == CONNECTED and i not in visited_node:
+                stack.append(i)
+                is_find = True
+                break
+        
+        if is_find == False:
+            _ = stack.pop()
+
+    return visited_node
 
 def bfs(n, m, v, g):
-    q = []
-    q.append(v) # queue initialize
     visited_node = [v]
+    q = [v]
 
-    while len(q): # loop until visited queue is not empty
-        cur = q[0]
-        del q[0]
+    while q: # loop until visited queue is not empty
+        cur = q.pop(0)
 
         for i in range(1, n + 1):
             if g[cur][i] == CONNECTED and i not in visited_node:
                 q.append(i)
                 visited_node.append(i)
-                
-                g[cur][i] = VISITED
-                g[i][cur] = VISITED
-    for l in g:
-        print(l)
 
     return visited_node
 
@@ -33,7 +45,8 @@ def bfs(n, m, v, g):
 if __name__ == '__main__':
     n, m, v = map(int, input().split())
     #g = [[0] * (n + 1)] * (n + 1) # shallow copy will not be work!
-    g = [[0 for _ in range(n+1)] for __ in range(n+1)]
+    #g = [[0 for _ in range(n+1)] for __ in range(n+1)]
+    g = [[0]*(n + 1) for i in range(n + 1)]
 
     for i in range(m):
         s, e = map(int, input().split())
@@ -41,5 +54,4 @@ if __name__ == '__main__':
         g[s][e] = CONNECTED
         g[e][s] = CONNECTED
     
-    bfs_res = bfs(n, m, v, g)
-    print(bfs_res)
+    solution(n, m, v, g)
