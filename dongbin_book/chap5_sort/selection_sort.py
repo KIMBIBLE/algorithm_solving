f147979e98eb139fa29b00752f2668c2f60fd5b9
Sungkyung Kim <jotun9935@gmail.com>
2021-06-17 15:06:49 +0900

    선택정렬 설명 추가

--- dongbin_book/chap5_sort/selection_sort.py
+++ dongbin_book/chap5_sort/selection_sort.py
@@ -1,15 +1,18 @@
 import random
 
-def selection_sort(data_to_sort):
-    for i in range(1, len(data_to_sort)):
-        for j in range(i, 0, -1):
-            if data_to_sort[j - 1] > data_to_sort[j]:
-                data_to_sort[j - 1], data_to_sort[j] = data_to_sort[j], data_to_sort[j -1]
-            
-            else:
-                break
-    
-    return data_to_sort
+def selection_sort(data):
+    # 선택된 최솟값과 swap할 대상의 인덱스
+    for i in range(len(data)):
+        # 아직 정렬되지 않은 데이터에 대해 최솟값 찾기 수행
+        min_idx = i # 가장 작은 원소의 인덱스
+        for j in range(i + 1, len(data)):
+            if data[min_idx] > data[j]:
+                min_idx = j
+
+        data[i], data[min_idx] = data[min_idx], data[i]
+
+    return data
+
 
 unsorted_data = [random.randint(0, 10) for _ in range(10)]
 print('before: {}'.format(unsorted_data))
