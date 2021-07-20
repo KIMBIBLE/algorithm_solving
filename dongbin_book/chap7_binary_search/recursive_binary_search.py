b64b78e873c6e06bb433730a38b07f879675e010
Sungkyung Kim <jotun9935@gmail.com>
2021-07-20 20:43:41 +0900

    debugging statement 추가

--- dongbin_book/chap7_binary_search/recursive_binary_search.py
+++ dongbin_book/chap7_binary_search/recursive_binary_search.py
@@ -14,23 +14,35 @@
 # Output 2
 Cannot find 7 in list
 """
-
+import traceback
 import sys
 readline = lambda: sys.stdin.readline().rstrip()
 
 def binary_search(array, target, start, end):
+    traceback.print_stack()
+    print('')
     if start > end:
         return None
 
     mid = (start + end) // 2
+    print("start: {}, mid: {}, end: {}".format(start, mid, end))
     if array[mid] == target:
+        print('find')
         return mid
     
     elif array[mid] > target:
-        return binary_search(array, target, start, mid - 1)
+        # return binary_search(array, target, start, mid - 1)
+        x = binary_search(array, target, start, mid - 1)
+        print(x)
+        return x
     
     else:
-        return binary_search(array, target, mid + 1, end)
+        #return binary_search(array, target, mid + 1, end)
+        x = binary_search(array, target, mid + 1, end)
+        print(x)
+        return x
+
+    print('1') # unreachable
 
 
 if __name__ == '__main__':
